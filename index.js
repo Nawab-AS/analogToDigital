3580925ebfa694a632a99bc4928b66a2027a418f
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-11-08 22:58:05 -0500

    added 1st clock

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const { createApp, ref } = Vue;
 
+const width = 80;
+const height = 20;
+
 const title = "Analog To Digital";
+const hands = ref([]);
+
+for (let w = 0; w < width; w++) {
+    hands.value.push([]);
+    for (let h = 0; h < height; h++) {
+        hands.value[w].push({ minute: 0, hour: 0 });
+    }
+}
 
 createApp({
     setup() {
-        return { title };
+        return { title, hands };
     }
 }).mount('body');
