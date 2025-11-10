cf3a2627a7effb42fcfb70b62873a1afa9affffd
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-11-09 21:18:09 -0500

    added selectable clock styles

---
+++ clockStyles.js
@@ -0,0 +1,93 @@
+const clockStyles = {
+    'vetical1': {
+        grid: {width: 8, height: 18},
+        hint: "Vertical (with seconds)",
+        onSecond: () => {
+            const now = new Date(Date.now());
+            
+            // hour
+            let hours = now.getHours()%12;
+            if (hours === 0) hours = 12;
+            displayNumber(hours, 0, 0);
+            
+            // minute
+            let minutes = now.getMinutes();
+            displayNumber(minutes, 0, 6);
+
+            // second
+            let seconds = now.getSeconds();
+            displayNumber(seconds, 0, 12);
+        },
+    },
+
+    
+    'vetical2': {
+        grid: {width: 8, height: 12},
+        hint: "Vertical (without seconds)",
+        onSecond: (updateClocks) => {
+            const now = new Date(Date.now());
+            
+            // hour
+            let hours = now.getHours()%12;
+            if (hours === 0) hours = 12;
+
+            displayNumber(hours, 0, 0);
+            
+            // minute
+            let minutes = now.getMinutes();
+            displayNumber(minutes, 0, 6);
+        },
+    },
+
+    
+    'traditional1': {
+        grid: {width: 28, height: 6},
+        hint: "Horizontal (with seconds)",
+        onSecond: (updateClocks) => {
+            const now = new Date(Date.now());
+            
+            // hour
+            let hours = now.getHours()%12;
+            if (hours === 0) hours = 12;
+            displayNumber(hours, 0, 0);
+            
+            // minute
+            let minutes = now.getMinutes();
+            displayNumber(minutes, 10, 0);
+
+            // second
+            let seconds = now.getSeconds();
+            displayNumber(seconds, 20, 0);
+            
+            // colons
+            displayColon(now.getSeconds()%2 == 1, 8, 0);
+            displayColon(now.getSeconds()%2 == 1, 18, 0);
+        },
+    },
+
+    
+    'traditional2': {
+        grid: {width: 28, height: 6},
+        hint: "Horizontal (without seconds)",
+        onSecond: (updateClocks) => {
+            const now = new Date(Date.now());
+            
+            // hour
+            let hours = now.getHours()%12;
+            if (hours === 0) hours = 12;
+            displayNumber(hours, 0, 0);
+            
+            // minute
+            let minutes = now.getMinutes();
+            displayNumber(minutes, 10, 0);
+
+            // second
+            let seconds = now.getSeconds();
+            displayNumber(seconds, 20, 0);
+            
+            // colons
+            displayColon(now.getSeconds()%2 == 1, 8, 0);
+            displayColon(now.getSeconds()%2 == 1, 18, 0);
+        },
+    },
+}
--- index.js
+++ index.js
@@ -3,27 +3,10 @@ const { createApp, ref, watch } = Vue;
 const randint = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const hands = ref([]);
 const clockSize = ref(40);
+const clockStyle = ref(Object.keys(clockStyles)[0]);
 let clockInterval;
 let width = ref(8);
 let height = ref(18);
-
-function onResize() {
-    if (clockInterval) clearInterval(clockInterval);
-    hands.value = [];
-    for (let h = 0; h < height.value; h++) {
-        hands.value.push([]);
-        for (let w = 0; w < width.value; w++) {
-            hands.value[h].push({ minute: randint(0, 39)/10,
-                hour: randint(0, 39)/10,
-                timingM: Math.random()*0.4 + 0.5,
-                timingH: Math.random()*0.4 + 0.5
-            });
-        }
-    }
-    startClockInterval();
-}
-onResize();
-watch([width, height], onResize);
 
 
 function updateClocks(number, x1, y1) {
@@ -35,35 +18,58 @@ function updateClocks(number, x1, y1) {
     }
 }
 
+function displayNumber(number, x1, y1) {
+    updateClocks(Math.floor(number/10), x1, y1);
+    updateClocks(number % 10, x1 + 4, y1);
+}
+
+function displayColon(visible, x1, y1) {
+    updateClocks(visible? 10:11, x1, y1);
+}
+
 function startClockInterval() {
     clockInterval = setInterval(()=>{
-        const now = new Date(Date.now());
-        
-        // hour
-        let hours = now.getHours()%12;
-        if (hours === 0) hours = 12;
-
-        updateClocks(hours < 10 ? 0 : Number(hours.toString()[0]), 0, 0);
-        updateClocks(hours < 10 ? hours : Number(hours.toString()[1]), 4, 0);
-        
-        // minute
-        let minutes = now.getMinutes();
-
-        updateClocks(minutes < 10 ? 0 : Number(minutes.toString()[0]), 0, 6);
-        updateClocks(minutes < 10 ? minutes : Number(minutes.toString()[1]), 4, 6);
-
-        // second
-        let seconds = now.getSeconds();
-
-        updateClocks(seconds < 10 ? 0 : Number(seconds.toString()[0]), 0, 12);
-        updateClocks(seconds < 10 ? seconds : Number(seconds.toString()[1]), 4, 12);
-    }, 900);
+        clockStyles[clockStyle.value].onSecond();
+    }, 250);
 }
+
+
+// initialize hands
+function initializeHands() {
+    hands.value = [];
+    for (let h = 0; h < height.value; h++) {
+        hands.value.push([]);
+        for (let w = 0; w < width.value; w++) {
+            hands.value[h].push({ minute: randint(0, 39)/10,
+                hour: randint(0, 39)/10,
+                timingM: Math.random()*0.4 + 0.5,
+                timingH: Math.random()*0.4 + 0.5
+            });
+        }
+    }
+}
+
+
+watch(clockStyle, (newStyle) => {
+    if (clockInterval) clearInterval(clockInterval);
+
+    width.value = clockStyles[newStyle].grid.width;
+    height.value = clockStyles[newStyle].grid.height;
+
+    initializeHands();
+    startClockInterval();
+});
+
+
+// start
+initializeHands();
+startClockInterval();
+
 
 // mount vue
 const app = createApp({
     setup() {
-        return { width, height, hands, clockSize };
+        return { width, height, hands, clockSize, clockStyle, clockStyles };
     }
 })
 
